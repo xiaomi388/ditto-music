739c794b9a10b0279e8477f9a7f1c3ca23e86052
xiaomi388 <xiaomi388@gmail.com>
2021-05-08 12:24:52 -0700

    Initial Commit

---
+++ Shared/Ditto_MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ditto_MusicApp.swift
+//  Shared
+//
+//  Created by 陈语梵 on 2021/5/8.
+//
+
+import SwiftUI
+
+@main
+struct Ditto_MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
